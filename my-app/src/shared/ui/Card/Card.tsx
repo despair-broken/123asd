b52e6fa30f0ea4f0f0ec733ba67954eb059174ad
Almaz <qweeeeezy@yandex.ru>
2026-05-27 10:06:27 +0300

    fixphoto

--- my-app/src/shared/ui/Card/Card.tsx
+++ my-app/src/shared/ui/Card/Card.tsx
@@ -1,13 +1,10 @@
 import type { Photo } from "../../types/photo.ts";
 import styles from "./Card.module.scss";
-
 type CardVariant = 'primary' | 'secondary' | 'success';
-
 interface CardProps {
     photo: Photo;
     variant?: CardVariant;
 }
-
 export const Card = ({ photo, variant = 'primary' }: CardProps) => {
     return (
         <div className={`${styles.card} ${styles[variant]}`}>
@@ -15,14 +12,20 @@ export const Card = ({ photo, variant = 'primary' }: CardProps) => {
                 className={styles.thumbnail}
                 src={photo.thumbnailUrl}
                 alt={photo.title}
+                onError={(e) => {
+                    e.currentTarget.src =
+                        `https://picsum.photos/600/400?random=${photo.id}`
+                }}
             />
-
             <img
                 className={styles.image}
                 src={photo.url}
                 alt={photo.title}
+                onError={(e) => {
+                    e.currentTarget.src =
+                        `https://picsum.photos/80/80?random=${photo.id}`
+                }}
             />
-
             <div className={styles.content}>
                 <p className={styles.id}>
                     #{photo.id}
@@ -31,10 +34,6 @@ export const Card = ({ photo, variant = 'primary' }: CardProps) => {
                 <h3 className={styles.title}>
                     {photo.title}
                 </h3>
-
-                <p className={styles.album}>
-                    Album: {photo.albumId}
-                </p>
             </div>
         </div>
     );
